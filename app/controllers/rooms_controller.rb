82553e282eadb5d697299803b4e24849382646ec
ogaki0612 <kog.v.0612@gmail.com>
2020-11-08 09:45:25 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,8 @@
 class RoomsController < ApplicationController
 
+  def index
+  end
+
   def new
     @room = Room.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-  root to:"messages#index"
+  root to:"rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
