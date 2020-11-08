b8beabc0dbc47f3a29aa7c2b1f09e4c9d01fe4c1
ogaki0612 <kog.v.0612@gmail.com>
2020-11-08 11:09:21 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'messages/index'
   root to:"rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+  resources :messages, only: [:index, :create]
+  end
 end
